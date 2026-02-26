c64e1e7238f9b8be75b3c31df1a5484a13eef235
MadJy <jeandavy1812@gmail.com>
2026-02-26 11:08:45 +0100

    Création des cases spéciales (tests) avec position (8 cases -> 2 de chaque ; position_caseRetour : 4,8,16,28 ; position_caseDe : 7,12,20,26)

--- src/modele/Plateau.java
+++ src/modele/Plateau.java
@@ -8,11 +8,17 @@ public class Plateau {
 
     public Plateau() {
     	//Sert à créer et positionner les cases speciales
-    	/*this.casesSpeciales = new CaseSpeciale[];
-        this.casesSpeciales[0] = new CaseRetour();
-        this.casesSpeciales[1] = new CaseDe();*/
+        this.casesSpeciales[0] = new CaseRetour(4);
+        this.casesSpeciales[1] = new CaseDe(7);
+        this.casesSpeciales[2] = new CaseRetour(8);
+        this.casesSpeciales[3] = new CaseDe(12);
+        this.casesSpeciales[4] = new CaseRetour(16);
+        this.casesSpeciales[5] = new CaseDe(20);
+        this.casesSpeciales[6] = new CaseRetour(24);
+        this.casesSpeciales[7] = new CaseDe(26);
         
         
+       
         this.pirates = new Pirate[2];
         this.pirates[0] = new Pirate("Jack Le Borgne", Couleur.ROUGE);
         this.pirates[1] = new Pirate("Bill Jambe De Bois", Couleur.BLEU);
